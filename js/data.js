6dc5ed9e25aba0c66d07594d07855246d0737794
k4t4str0f4@gmail.com <coffeemelody72@gmail.com>
2024-03-25 19:59:41 +0500

    8.14

--- js/data.js
+++ js/data.js
@@ -30,3 +30,70 @@ const createPhotos = (a)=> {
 for(let i = 1; i <= PHOTOS; i++){
   arrayPhotos.push(createPhotos(i));
 }
+
+const pictureNode = document.querySelector('.picture');
+const pictureTemplate = document.querySelector('.#picture').textContent.querySelector('.picture');
+const pictureFragment = document.createDocumentFragment();
+
+photos.forEach.(({id, url, comments, likes}) => {
+  const pictureNode = pictureTemplate.cloneNode(true);
+
+  pictureNode.dataset.pictureId = id;
+  pictureNode.querySelector('picture__img').src = url;
+  pictureNode.querySelector('.picture__comments').textContent = comments.length;
+  pictureNode.querySelector('.picture__like').textContent.like;
+
+  pictureFragment.appendChild(pictureNode);
+});
+
+pictureNode.appendChild(pictureFragment)
+
+export {pictureNode}
+
+
+const onBigPictureCancelClick = () => {
+closeBigPicture();
+}
+
+const onEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    closeBigPicture();
+  }
+};
+
+const closeBigPicture = () => {
+bigPictureNode.classList.add('hidden');
+bigPictureClasseNode.removeEventLister('click, oneBigPictureCancelClick');
+document.removeEventListener('Keydown', onEscKeydown);
+};
+
+const openBigPicture = (pictureID) => {
+const currentPhoto = photos.find((photos) => photos.id === Number(pictureId));
+const socialCommentsFragmetn = document.createDocumentFragment();
+
+bigPictureImgNode.src = currentPhoto.url;
+likeCountNode.textContent = currentPhoto.like;
+socialCommentsNode.innerHTML = '';
+
+currentPhoto.comments.forEach((comment) => {
+  const socialCommentsNode = socialCommentTempalte.cloneNode(true);
+
+  socialCommentNode.querySelector('.social__picture').src = comment.avatar;
+  socialCommentNode.querySelector('.social__picture').alt = comment.name;
+  socialCommentsNode.querySelector('.social__text').textContent = comment.message;
+
+  socialCommentsFragmetn.appendChild(socialCommentNode);
+});
+
+socialCommentNode.appendChild(socialCommentsFragmetn);
+commentsCaptionNode.textContent = currentPhoto.description;
+commentsCountNode.classList.add('hidden');
+commentsLoaderNode.classList.add('hidden');
+
+bigPictureNode.classList.remove('hidden');
+bigPictureCancelNode.addEventLister('click', onBigPictureCancelClick);
+document.body.classList.add('modal-open');
+document.addEventListener('keydown', onEscKeydown);
+}
+
+export {openBigPicture};
