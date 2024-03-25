11f1ab654c4b6650576cf26ccab55be0fe7a4e7a
k4t4str0f4@gmail.com <coffeemelody72@gmail.com>
2024-03-25 20:07:29 +0500

    8.14

--- js/main.js
+++ js/main.js
@@ -1,67 +1,12 @@
-const discription = ['Бывают моменты' , 'кода не хочется ни длинных строк' , 'ни философских рассуждений.Просто пишешь: Офигенно!', 'и бежишь жить дальше.'];
+import {renderPhotos} from './picture.js';
 
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-
-const MESSAGE = ['Всё отлично!', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Но не всё.'];
-const NAMES = ['Tanya', 'Julia', 'Alina', 'Katya', 'Johan'];
-
-const PHOTOS = 25;
+renderPhotos();
 
 import { pictureNode } from './data.js';
 
-<<<<<<< Updated upstream
-
-const generateRandomID = (a, b) =>{
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  return Math.floor(Math.random() * (upper - lower + 1) + lower);
-}
-const createComments = (a)=> {
-  return {
-    id: a,
-    avatar: `img/avatar-${generateRandomID(1,6)}.svg`,
-    MESSAGE: MESSAGE[generateRandomID(0,MESSAGE.length - 1)],
-    name: NAMES[generateRandomID(0,NAMES.length - 1)],
-  }
-};
-
-
-const arrayComments = [];
-for(let i = 1; i <= generateRandomID(1, 30); i++){
-  arrayComments.push(createComments(i));
-}
-
-const arrayPhotos = [];
-const createPhotos = (a)=> {
-  return {
-    id: a,
-    url: `photos/${a}.jpg`,
-    description: discription[generateRandomID(0,3)],
-    likes: generateRandomID(MIN_LIKES,MAX_LIKES),
-    comments: arrayComments,
-  }
-};
-for(let i = 1; i <= PHOTOS; i++){
-  arrayPhotos.push(createPhotos(i));
-}
-
-
-
-console.log(arrayPhotos);
-=======
 pictureNode();
 
 pictureNode.addEventListener('click', (evt) => {
 const currentPictureNode = evt.target.closest('.picture');
 
-if (currentPictureNode) {
-  openBigPicture(currentPictureNode.dataset.pictureid);
-}
-
 });
-
-import { openBigPicture } from './data.js';
-
-openBigPicture();
->>>>>>> Stashed changes
